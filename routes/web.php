b28ff7e4ba10d446452b93be580b53dd9b7e8a5f
Yuvraj Singh Solanki <111236237+yuvraj-solanki-2406@users.noreply.github.com>
2023-04-08 22:34:58 +0530

    multi auth with Admin

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', function () {
-    return "hello";
-});
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -33,3 +29,16 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+})->middleware(['auth:admin', 'verified'])->name('admin.dashboard');
+
+Route::middleware('auth:admin')->group(function () {
+    Route::get('admin/profile', [ProfileController::class, 'edit'])->name('admin.profile.edit');
+    Route::patch('admin/profile', [ProfileController::class, 'updateAdmin'])->name('admin.profile.update');
+    Route::delete('admin/profile', [ProfileController::class, 'destroy'])->name('admin.profile.destroy');
+});
+
+require __DIR__.'/adminauth.php';
